9357a3e6b34440960d6d320765c92d8ff47fb3b9
Pedro Freitas <Pedrofreitas5707@gmail.com>
2026-04-22 19:33:57 -0300

    fix: adiciona created_at na tabela users no V1

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
-    id   BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS products (
